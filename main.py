dca8900f12d2554a0d8b60a972d52ce00254416a
Alex Le <alexandretuanle@gmail.com>
2025-07-05 18:16:30 -0700

    added a way to track  the total profit every year

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from flask import Flask, request, render_template, redirect, url_for, Response, 
 base_income = 420.08 
 child_income = 210.04
 
+money_per_year = []
+
+def reset_money_per_year():
+    global money_per_year
+    money_per_year = []
+
 
 def initial_investment(years_married, number_of_children):
     total_income = base_income + number_of_children * child_income
-    initial_investment = 0
-    initial_investment += total_income
+    initial_investment = total_income
+    money_per_year.append(round(initial_investment, 2))  # Year 1
 
     for year in range(years_married - 1):
         initial_investment *= 1.126
         total_income *= 1.0286
         initial_investment += total_income
+        money_per_year.append(round(initial_investment, 2))  # Year 2, 3, ...
 
     return initial_investment
 
 
 def total_profit(years_invested, initial_investment):
-    total_profit = 0
-    total_profit += initial_investment
-
+    total_profit = initial_investment
     for year in range(years_invested):
         total_profit *= 1.126
-
+        money_per_year.append(round(total_profit, 2))
     return total_profit
 
 
@@ -31,6 +36,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    reset_money_per_year()
     return render_template('index.html')
 
 
@@ -47,7 +53,7 @@ def calculate():
     profit = round(profit, 2)
 
     return render_template('calculate.html', investment=investment, profit=profit, years_married=num_years_married, 
-        children=num_children, years_invested=num_years_invested) 
+        children=num_children, years_invested=num_years_invested, money_per_year=money_per_year) 
 
 
 @app.route('/graphs')
